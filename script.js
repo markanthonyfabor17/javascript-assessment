2547137a1bdbaa9f8eeefc08960c25ad4d65a951
Mark Anthony Fabor <markanthonyfabor17@gmail.com>
2023-04-05 20:59:21 +0800

    assessment

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ console.log("connected");
 
 // Coding Exam
 //1. Person Details
+/* 
 console.log("Hello World!");
 
 const firstName = "John";
@@ -52,6 +53,8 @@ ${isMarried}`)
 }
 hisRelationship();
 
+*/
+
 // let number = 0;
 // for (let i = 20; i >= number; i++) {
 //     for (let j = 20; i >= number; j--) {
@@ -71,6 +74,108 @@ hisRelationship();
 // else {
 //     console.log(giveNumber);
 // }
+
+
+// let numOne = 10;
+// let numTwo = 20;
+
+// //using assignment operators
+// numOne += numTwo;
+// console.log(numOne);
+// numOne -= numTwo;
+// console.log(numOne);
+// numOne *= numTwo;
+// console.log(numOne);
+// numOne /= numTwo;
+// console.log(numOne);
+
+// //comparison operators
+// if (numOne <= numTwo) {
+//     console.log("numOne is less than numTwo");
+// }
+// else {
+//     console.log("numOne is greater than numTwo")
+// }
+
+
+// if (numOne !== numTwo) {
+//     console.log(true)
+// }
+// else {
+//     console.log(false)
+// }
+
+
+// let typeColor = prompt("Choose with color: Black, Violet and Pink");
+
+// switch (typeColor) {
+//     case "Black":
+//         console.log("You chose Black");
+//         break;
+//     case "Violet":
+//         console.log("You chose Violet");
+//         break;
+//     case "Pink":
+//         console.log("You chose Pink");
+//         break;
+//     default:
+//         console.log("You chose something else");
+
+// }
+
+// let oddOrEven = prompt("Enter any number");
+
+// function isEven(oddOrEven) {
+//     if (oddOrEven % 2 == 0) {
+//         console.log("The number is even");
+//     }
+//     else {
+//         console.log("The number is odd");
+//     }
+// }
+// isEven();
+
+// let num = prompt("Enter any number: ")
+
+// for (let x = 0; x <= num; x++) {
+//     if (x % 2 === 0) {
+//         console.log(x + " is even");
+//     }
+
+// }
+
+let num = prompt("Give me a number: ");
+
+
+for (let x = 50; x >= num; x++) {
+    if (username == "") {
+        console.log(x + " is even");
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
